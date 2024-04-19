bdc7320aff8ec7017a7bce20c179cb37b064a6b3
Sonu Raj <65347096+sonu267@users.noreply.github.com>
2024-04-19 15:35:54 +0530

    Add files via upload
    giving grade of a student according to their percentage

---
+++ marks.py
@@ -0,0 +1,13 @@
+marks= int(input("enter your percentage: "))
+if (marks > 90):
+    print("grade \"A\"")
+elif(marks > 80 and marks <= 90 ):
+    print("grade \"B\" ")
+elif(marks >= 60 and marks <= 80 ):
+    print("grade \"C\" ")
+elif(marks < 60 ):
+    print("grade \"D\" ")
+    
+
+    
+    
